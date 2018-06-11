a14cbf53beb44b6657d7291af150abc42f6b0fca
jesp6763 <jesp6763@edu.campusvejle.dk>
2018-06-11 10:33:35 +0200

    Fixed a bunch of errors. Now the products gets listed.

--- js/View/Bikes.js
+++ js/View/Bikes.js
@@ -1,20 +1,28 @@
 /**
- * Responsible for rendering the Bikes page.
+ * Responsible for everything that has to do with the Bikes page.
  */
 class Bikes {
     static get Template() {
         return this.template;
     }
 
+    static set Template(value) {
+        this.template = value;
+    }
+
     static get Modal() {
         return this.modal;
+    }
+
+    static set Modal(value) {
+        this.modal = value;
     }
 
     static SetupUserInterface() {
         let modalElement = document.getElementById('bicycleSpecsModal');
 
-        this.template = document.getElementById('bicycle-product');
-        this.modal = {
+        Bikes.Template = document.getElementById('bicycle-product');
+        Bikes.Modal = {
             element: modalElement,
             title: modalElement.querySelector('#bicycle-product-title'),
             content: modalElement.getElementsByClassName('modal-body')[0]
@@ -32,25 +40,27 @@ class Bikes {
         let bikeInstanceKeys = Object.keys(Bike.Instances);
         
         for (let i = 0; i < bikeInstanceKeys.length; i++) {
-            const bike = Bike.Instances[i];
+            const bike = Bike.Instances[bikeInstanceKeys[i]];
 
             let productClone = document.importNode(Bikes.Template.content, true);
-            let buttons = productClone.getElementsByTagName('button');
+            let seeSpecsButton = productClone.querySelector('button');
 
             let productData = {
-                title: productClone.getElementsByClassName('card-title')[0],
-                price: productClone.getElementsByClassName('bike-price')[0],
-                colors: productClone.getElementsByClassName('bike-color'),
-                seeMoreBtn: buttons[buttons.length - 1]
+                title: productClone.querySelector('.card-title'),
+                price: productClone.querySelector('.bike-price'),
+                colors: productClone.querySelector('.bike-color'),
+                seeSpecsBtn: seeSpecsButton
             }
 
             productData.title.innerHTML = bike.Name;
             productData.price.innerHTML = "$".concat(bike.Price);
 
-            productData.seeMoreBtn.addEventListener('click', function(){
+            productData.seeSpecsBtn.addEventListener('click', function(){
                 this.RefreshSpecsModal(bike);
+                console.log("wdla");
             });
-            parentNode.appendChild(productClone);
+
+            document.querySelector('.products-list').appendChild(productClone);
         }
     }
 
